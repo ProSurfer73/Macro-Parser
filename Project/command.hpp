d09b3e92936b7d9e191657ecefb95b6bd2d86cf2
Frederic Ruelle <frederic.ruelle@st.com>
2022-09-06 15:33:01 +0200

    docs: license updated
    As this tool has been developed as one of the assignments
    of the apprenticeships, it must have the ST copyright.
    It must also be compliant with the ST licensing scheme.

--- Project/command.hpp
+++ Project/command.hpp
@@ -1,3 +1,22 @@
+/**
+  ******************************************************************************
+  * @file    command.hpp
+  * @author  MCD Application Team
+  * @brief   Macro-Parser
+  *
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
 #ifndef COMMAND_HPP
 #define COMMAND_HPP
 
--- Project/config.hpp
+++ Project/config.hpp
@@ -1,3 +1,22 @@
+/**
+  ******************************************************************************
+  * @file    config.hpp
+  * @author  MCD Application Team
+  * @brief   Macro-Parser
+  *
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
 
