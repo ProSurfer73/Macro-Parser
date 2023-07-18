e71081b9889b911740aaf1dd4967b3502896ff65
unknown <jdivouxbret73@gmail.com>
2023-07-18 16:01:37 +0200

    fix: fixed warning. - fixeda warning from visual sudio ide from appearing. - it is related to the following issue: https://github.com/ProSurfer73/Macro-Parser/issues/77.

--- Project/stringeval.cpp
+++ Project/stringeval.cpp
@@ -323,7 +323,7 @@ double evaluateSimpleArithmeticExpr(const string& expr)
     }
     if(isOperationCharacter(expr.front()))
         nbOperators--;
-    char* operators = (char*)new int8_t[nbOperators*sizeof(char)+(nbOperators+1)*sizeof(double)]; // operators.size => nbOperators
+    char* operators = (char*)new int8_t[nbOperators*sizeof(char)+(nbOperators+1/* safety*/+1) * sizeof(double)]; // operators.size => nbOperators
     double *numbers = (double*)((int8_t*)operators+sizeof(char)*nbOperators); // numbers.size => nbOperators+1.
 
     std::istringstream mathStrm(expr);
@@ -336,7 +336,7 @@ double evaluateSimpleArithmeticExpr(const string& expr)
         ++pos;
 
         if (pos >= nbOperators)
-            throw std::runtime_error("no time for that.");
+            throw std::runtime_error("internal error line 339: post qn issue on github.");
     }
 
     while(nbOperators > 0)
@@ -720,7 +720,7 @@ std::vector<std::string>* printWarnings, bool enableBoolean, std::vector<std::st
                             bool oneThing=false;
 
                             auto range = dictionary.equal_range(p.first);
-                            for(auto  it2=range.first; it2!=range.second; it2++)
+                            for(auto it2=range.first; it2!=range.second; it2++)
                             {
                                 const auto& pp = *it2;
 
